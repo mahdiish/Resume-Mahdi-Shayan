6654673e3d659e25a4af72560a43a3c7e8bdf213
mahdiish <mehdishayan1119@gmail.com>
2023-04-09 23:14:10 +0430

    Second section created

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menuItem = document.querySelectorAll(".menu-item");
+const menuContent = document.querySelectorAll(".menu-content");
 document.addEventListener("click", function (e) {
   let element = e.target;
   if (element.classList[0] == "menu-item") {
@@ -6,9 +7,27 @@ document.addEventListener("click", function (e) {
       if (menuItem[index] != element) {
         menuItem[index].classList.remove("selected-menu-item");
         menuItem[index].classList.add("unselected-menu-item");
+        menuContent[index].classList.add("none-display");
+      } else {
+        element.classList.remove("unselected-menu-item");
+        element.classList.add("selected-menu-item");
+        menuContent[index].classList.remove("none-display");
       }
     }
-    element.classList.remove("unselected-menu-item");
-    element.classList.add("selected-menu-item");
   }
 });
+
+// Typing Effect
+var i = 0;
+var txt = "R Front-End Developer";
+typeWriter();
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector(".typing-effect").innerHTML += txt[i];
+    i++;
+  } else if (i == txt.length) {
+    document.querySelector(".typing-effect").innerHTML = "J";
+    i = 0;
+  }
+  setTimeout(typeWriter, 200);
+}
